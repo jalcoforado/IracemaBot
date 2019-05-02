978a22438a4e7c187159a049c94859fc3cdca3c6
jalcoforado <jalcoforado@gmail.com>
2019-05-01 23:05:38 -0300

    Meu primeiro gi

--- src/core.py
+++ src/core.py
@@ -1,9 +1,20 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 from conf.settings import TELEGRAM_TOKEN, HTTP_CATS_URL
+from datetime import datetime
+import logging
 
-Localizacao = []
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+Localizacao = {}
 def start(bot, update):
+    print("Start")
     response_message = "=^._.^="
+   
     bot.send_message(
         chat_id=update.message.chat_id,
         text=response_message
@@ -20,7 +31,8 @@ def http_cats(bot, update, args):
 def avisar(bot, update, args):
     response_message = "Ok "+update.message.from_user.first_name+u", até ja!"
     global Localizacao
-    Localizacao[update.message.from_user.first_name] = args[0:]
+    Localizacao[update.message.from_user.first_name] = (datetime.now,args[0:])
+    print(Localizacao)
     bot.send_message(
         chat_id=update.message.chat_id,
         text=response_message
@@ -28,7 +40,7 @@ def avisar(bot, update, args):
 
 def ondeesta(bot, update, args):
     global Localizacao
-    response_message = Localizacao[update.message.from_user.first_name] 
+    response_message = Localizacao[args[0]] 
     bot.send_message(
         chat_id=update.message.chat_id,
         text=response_message
@@ -45,32 +57,30 @@ def unknown(bot, update):
 
 
 def main():
-    try:
-        updater = Updater(token=TELEGRAM_TOKEN)
+    
+    updater = Updater(token=TELEGRAM_TOKEN)
 
-        dispatcher = updater.dispatcher
+    dispatcher = updater.dispatcher
 
-        dispatcher.add_handler(
-            CommandHandler('start', start)
-        )
-        dispatcher.add_handler(
-            CommandHandler('avisar', avisar, pass_args=True)
-        )
-        dispatcher.add_handler(
-            CommandHandler('ondeesta', ondeesta, pass_args=True)
-        )
-        dispatcher.add_handler(
-            CommandHandler('http', http_cats, pass_args=True)
-        )
-        dispatcher.add_handler(
-            MessageHandler(Filters.command, unknown)
-        )
+    dispatcher.add_handler(
+        CommandHandler('start', start)
+    )
+    dispatcher.add_handler(
+        CommandHandler('avisar', avisar, pass_args=True)
+    )
+    dispatcher.add_handler(
+        CommandHandler('ondeesta', ondeesta, pass_args=True)
+    )
+    dispatcher.add_handler(
+        CommandHandler('http', http_cats, pass_args=True)
+    )
+    dispatcher.add_handler(
+        MessageHandler(Filters.command, unknown)
+    )
 
-        updater.start_polling()
+    updater.start_polling()
 
-        updater.idle()
-    except expression as identifier:
-        print (expression.message)
+    updater.idle()
     
 
 
